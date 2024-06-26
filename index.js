96e4e9bcf84c2cb0f45b1d7d8ca83d089e613b72
Farida Yeasmin <sohanamou25@yahoo.com>
2024-06-26 12:54:49 +0300

    connected with mongodb atlas database

--- index.js
+++ index.js
@@ -1,134 +1,80 @@
-//(without express)the application imports Node's built-in Web server module (in our browser
-//side code we write it like this "import http from 'http")
-//const http = require("http")
+require('dotenv').config()
+
 const express = require('express');
-const morgan = require('morgan');
-//importing express, which this time is a function that is used to create an Express application stored in the app variable:
 const cors = require('cors')
-//To make Express show static content, the page index.html and the JavaScript, etc., it fetches, we need a built-in middleware from Express called static.
-
-
+const Person = require('./models/person')
 const app = express();
 // Middleware to parse JSON bodies
 app.use(express.json())
+
+//To make Express show static content, the page index.html and the JavaScript, etc., it fetches, we need a built-in middleware from Express called static.
 app.use(express.static('dist')) 
 app.use(cors())
-const generateId = () => {
-  return Math.floor(Math.random() * 1000000)
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
+app.use(requestLogger)
+let persons = [
+]
 
+const unknownEndpoint = (request, response)=>{
+  response.status(404).send({error:'unknown endpoint'})
 
-//To define a token, simply invoke morgan.token() with the name 
-//and a callback function. This callback function is expected to 
-//return a string value. The value returned is then available as ":post-body" 
-//in this case:
-morgan.token('post-body',(request, response) => {
-  if (request.method === 'POST'){
-    return JSON.stringify(request.body)
-  }
-  return '';
+}
+app.get('/', (request, response)=>{
+  response.send('<h1>Hello World</h1>')
+
 })
-
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :post-body')
-)
-let persons = 
-    [
-              { 
-                "id": 1,
-                "name": "Arto Hellas", 
-                "number": "040-123456"
-              },
-              { 
-                "id": 2,
-                "name": "Ada Lovelace", 
-                "number": "39-44-5323523"
-              },
-              { 
-                "id": 3,
-                "name": "Dan Abramov", 
-                "number": "12-43-234345"
-              },
-              { 
-                "id": 4,
-                "name": "Mary Poppendieck", 
-                "number": "39-23-6423122"
-              }
-            ]
-
-//(without express)The code uses the createServer method of the http module to create a new
-// web server. An event handler is registered to the server that is called every time an HTTP request is made to the server's address http://localhost:3001.
-//The request is responded to with the status code 200, with the Content-Type 
-//header set to application/json, and the content of the site to be returned set to persons.
-//The last rows bind the http server assigned to the app variable, 
-//to listen to HTTP requests sent to port 3001
-//const app = http.createServer((request, response) => {
-    //response.writeHead(200, { 'Content-Type': 'application/json' })
-
-    //response.end() method expects a string or a buffer to send as the 
-    //response body.
-    //response.end(JSON.stringify(persons))
-  //})
-
 app.get('/api/persons',(request, response) =>{
-  console.log("/api/persons get")
-  response.json(persons)
+  const body = request.body
+  if (body.content === undefined){
+    return response.status(400).json({error: 'content missing'})
+  }
+  const person = new Person({
+    name: body.name,
+    number: body.number 
+  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => { //app.get
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-})
-
-app.get('/info',(request, response)=> {
-  const currentTime = new Date().toLocaleString();
-  const numberOfEntries = persons.length;
-  response.send(`
-        <p>The request was received at: ${currentTime}</p>
-        <p>Phonebook has info for ${numberOfEntries} people</p>
-    ` 
-  )
-}) 
-
-app.delete('/api/persons/:id', (request,response) =>{
-  const id = Number(request.params.id)
-  const person = persons.find(persons => persons.id === id)
-  if (person){
-    const personIndex = persons.findIndex(persons => persons.id === id)
-    persons.splice(personIndex, 1)
+app.get('/api/persons/:id', (request, response) => { 
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } 
-  else
-    response.status(404).end()
-
+  })
 })
-
 
 app.post('/api/persons', (request, response) =>{
-  const {name, number} = request.body;
-  console.log("/api/persons post", name)
-  if (!name || !number) {
-    return response.status(400).send({message:'Name and number are required.' })
+  const body = request.body;
+  if (body.content === undefined){
+    return response.status(400).json({error: 'content missing'})
   }
-  const existingName = persons.find(newNameEntry => newNameEntry.name === name);
-  if(existingName){
-    return response.status(404).send({error: 'Name must be unique.'})
-  }
-  const newEntry = {
-    id: generateId(),
-    name,
-    number
-  }
-  persons.push(newEntry);
-  response.status(201).send(newEntry);
-});
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
 
-const PORT = process.env.PORT || 3001
+app.delete('/api/persons/:id', (request,response) =>{
+  const id = Number (request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.use(unknownEndpoint)
+
+
+
+
+const PORT = process.env.PORT
 app.listen(PORT, ()=>{
   console.log(`Server running on port ${PORT}`)
 })
